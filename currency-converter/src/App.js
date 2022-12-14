569b1fe9d4c4d327b39cf36f92acaf0739133db9
gloria oduor <gloriaoduor@gmail.com>
2022-12-14 16:38:40 +0300

    functionality done

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -7,27 +7,52 @@ import { format } from "date-fns";
 function App() {
 
   const [source, setSource] = useState ("usd");
-  const [currOptions, setCurrOptions] = useState (" ");
+  const [destination, setDestination] = useState ("");
+  const [currOptions, setCurrOptions] = useState ("");
+  const [input, setInput] = useState("");
+  const [total, setTotal] =useState("")
+  
 
   //fetch all currencies
   useEffect(() => {
-    Axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json`)
-      .then((res) => 
-        {setCurrOptions(res.data);}
+    Axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${source}.json`)
+      .then((response) => setCurrOptions(response.data[source])
       )}, [source]); 
+    // console.log(currOptions[destination])
+    
 
   //to get the  conversion rate, append the source/destination currency to the url then .json
 
   //map the object into an array of values/indexes to display in the selection list
-  const options = Object.values(currOptions).map((value, index)=>
+  const options = Object.keys(currOptions).map((value, index)=>
       <option key={index}>
         {value}
       </option>
     );
+  
+  //extract rate 
+  var rate = currOptions[destination]
+  // console.log(rate)
+
+  const handleSourceChange = (e) => {
+    setSource(e.target.value)
+  }
+  const handleDestChange = (e) => {
+    setDestination(e.target.value)
+  }
+  
+  // console.log(source + " " + destination)
+
+  //fxn to convert
+  function convert(){
+    setTotal(input * rate)
+    console.log(total);
+  }
+ 
 
 
   return (
-   <div className='App pt-32'>
+   <div className='App'>
       <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
       <div className='Container bg-sky-100 border-4 rounded-md p-6'>
         <div className='Heading '>
@@ -39,14 +64,18 @@ function App() {
               {/* below div needs to be centered */}
               <div className='Dropdown pl-6'> 
                 <h4>From:</h4>
-                <select className='drop outline outline-2 rounded p-2'>
+                <select className='drop outline outline-2 rounded p-2' 
+                  onChange={handleSourceChange}
+                >
                   {options}
                 </select>
               </div>
               
               <div className='Dropdown'>
                 <h4>To:</h4>
-                <select className='drop outline outline-2 rounded p-2'>
+                <select className='drop outline outline-2 rounded p-2'
+                onChange={handleDestChange}
+                >
                   {options}
                 </select>
               </div>
@@ -56,19 +85,23 @@ function App() {
               <div className='Sourcefield'>
                 <h3>Amount:</h3>
                 <input type='text' className='TextBox outline outline-2 rounded px-4  '
-                      placeholder='Amount to convert'/>
+                      placeholder='Amount to convert'
+                      onChange={(e)=> setInput(e.target.value)}
+                      />
               </div>
 
               <div className='Destinationfield'>
                 <h3>Conversion rate: </h3>
-                <p> xxxxx </p>
+                <p> {rate} </p>
               </div>
             
               <div className='Destinationfield'>
                 <h3>Converted Amount: </h3>
-                <p> xxxxx </p>
+                <p> {total} </p>
               </div>
-              <button className='Button p-2 mt-4 rounded bg-orange-200 hover:bg-orange-400 outline outline-2 outline-orange-300'> Convert</button>
+              <button className='Button p-2 mt-4 rounded bg-orange-200 hover:bg-orange-400 outline outline-2 outline-orange-300'
+                onClick={convert}
+              > Convert</button>
 
             </div>
         </div>
